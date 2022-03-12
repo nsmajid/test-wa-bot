93fce2f53da488db952c107e3018d84afed156c9
Nursyam Majid <nursyammajid@gmail.com>
2022-03-12 14:50:40 +0700

    update session

--- app.js
+++ app.js
@@ -28,7 +28,18 @@ const client = new Client({
     authStrategy: new LegacySessionAuth({
         session: sessionData
     }),
-    puppeteer: { args: ["--no-sandbox"] }
+    puppeteer: {
+        headless: true,
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process', // <- this one doesn't works in Windows
+            '--disable-gpu'
+        ], }
 });
 client.initialize();
 
